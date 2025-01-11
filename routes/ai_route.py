041fb3e32a2cb42d26b77b78f3eb54cd8b3680cc
baaak1234 <zxcv2103@naver.com>
2025-01-11 15:46:55 +0900

    DB_mode_connect

--- routes/ai_route.py
+++ routes/ai_route.py
@@ -5,13 +5,16 @@ import joblib
 import numpy as np
 import pandas as pd
 
+# route 설정
 ai_route = Blueprint('ai',__name__)
-
+# model 불러오기
 lin_reg = joblib.load('house_price_model_lin.pkl')
 rf_reg = joblib.load('house_price_model_rf.pkl')
 
 
 @ai_route.route("/predict-house-price",methods=['get'])
+
+# 함수지정
 def predictHouseprice():
     
     area = request.args.get('area')
@@ -21,7 +24,7 @@ def predictHouseprice():
     school_rating = request.args.get('school_rating')
     transit_score = request.args.get('transit_score')
 
-
+    # feature 설정
 
     features = np.array([[
         int(area),
@@ -32,9 +35,29 @@ def predictHouseprice():
         int(transit_score),
     ]])
 
+    # 예측 실행
     lin_reg_pred = lin_reg.predict(features)[0]
     rf_reg_pred = rf_reg.predict(features)[0]
 
+    #DB연결
+    conn=get_db_connection()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    #SQL 쿼리 작성
+
+    query = """
+    INSERT INTO house
+    (area,rooms,year_built,income,school_rating,transit_score,created_date,pred_lin,pred_rf)
+    VALUES
+    (%s,%s,%s,%s,%s,%s,sysdate(),%s,%s)
+    """
+    cursor.execute(query,(area,rooms,year_built,income,school_rating,transit_score,lin_reg_pred,rf_reg_pred))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+#결과 도출
     return jsonify({
         "message":"ok",
         "price_by_lin": lin_reg_pred,
